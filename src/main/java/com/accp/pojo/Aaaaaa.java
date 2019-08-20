6c4a93e61ecf643ab193f70b036246474409ca3e
q2968203950 <2968203950@qq.com>
2019-08-20 15:57:56 +0800

    逆向工程搭建实体类

---
+++ src/main/java/com/accp/pojo/Aaaaaa.java
@@ -0,0 +1,11 @@
+package com.accp.pojo;
+
+/**
+ * @program: y2FirstProjects
+ * @description: Aaaaaa
+ * @author: hjn
+ * @create: 2019-08-20 15:57
+ **/
+public class Aaaaaa {
+    private int aaaaa;
+}
